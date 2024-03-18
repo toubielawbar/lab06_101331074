12c28bfa827f935e829602a374f7a8085499bf97
toubielawbar <toubielawbar@gmail.com>
2024-03-17 23:59:30 -0400

    LAB EXERCISE 6

---
+++ exercise-1/myapp/routes/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/', function(req, res, next) {
+  console.log(`First Name: ${req.body.firstName}`);
+  console.log(`Last Name: ${req.body.lastName}`);
+  res.send('POST received');
+});
+
+router.post('/submit-form', (req, res) => {
+  const formData = req.body;
+
+  console.log('Form data:', formData);
+
+  res.send('POST received!');
+});
+
+router.use(bodyParser.urlencoded({extended: true}));
+
+
+
+module.exports = router;
